87742560208b060d0d5cd3bc69a85150ac8773d0
EvanRouse <erouse91@DESKTOP-THRLDUE.localdomain>
2022-04-13 23:37:12 -0400

    commit tree 42f5fb911147a337fa9f7295fc36889b42cacd1a

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+};
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+};
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+};
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+};
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function appendCat(name) {
+ const copyOfCats = [
+    ...cats.slice(),
+    "Broom",
+  ];
+};
